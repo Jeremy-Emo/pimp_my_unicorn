87e6a182267337e7af6f690dfc9b26ad83133c48
D.LOISON <dorian.loison@viacesi.fr>
2018-11-19 16:25:55 +0100

    Add DTO and get data

--- PimpMyUnicorn/WinFormPimpMyUnicornClient/InterfaceClient.cs
+++ PimpMyUnicorn/WinFormPimpMyUnicornClient/InterfaceClient.cs
@@ -1,50 +1,73 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinFormPimpMyUnicornClient.DTO;
 
 namespace WinFormPimpMyUnicornClient
 {
     public partial class InterfaceClient : Form
     {
+        private string _connString = ConfigurationManager.ConnectionStrings["PimpMyUnicornDB"].ConnectionString;
+        private List<PartiesDTO> _parties = new List<PartiesDTO>();
+        private List<ElementsDTO> _elements = new List<ElementsDTO>();
+
         public InterfaceClient()
         {
             InitializeComponent();
+
+            using (MySqlConnection cn = new MySqlConnection(_connString))
+            {
+                cn.Open();
+                string query = "SELECT Id_partie, partielibelle FROM t_parties";
+                using (MySqlCommand cmd = new MySqlCommand(query, cn))
+                {
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            _parties.Add(new PartiesDTO
+                            {
+                                ID = Convert.ToInt32(dr.GetValue(0)),
+                                Libelle = dr.GetValue(1).ToString()
+                            });
+                        }
+                    }
+                }
+
+                query = "SELECT Id_element, partie_id, elementLibelle, elementsImg FROM t_elements";
+                using (MySqlCommand cmd = new MySqlCommand(query, cn))
+                {
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            _elements.Add(new ElementsDTO
+                            {
+                                ID = Convert.ToInt32(dr.GetValue(0)),
+                                PartieID = Convert.ToInt32(dr.GetValue(1)),
+                                Libelle = dr.GetValue(2).ToString(),
+                                Image = dr.GetValue(3).ToString()
+                            });
+                        }
+                    }
+                }
+            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private static Image DisplayBase64Picture(string base64)
         {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click_2(object sender, EventArgs e)
-        {
-
-        }
-
-        private void InterfaceClient_Load(object sender, EventArgs e)
-        {
-
+            Image newPicture;
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(base64))) { newPicture = Image.FromStream(ms); }
+            return newPicture;
         }
     }
 }
