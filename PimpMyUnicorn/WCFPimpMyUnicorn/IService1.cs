9abfba6aa203013a555bfda1f11db21cb733f33c
redcreep76 <lucien.brouillaud@wanadoo.fr>
2018-11-19 15:13:45 +0100

    Fonction update BDD WCF

--- PimpMyUnicorn/WCFPimpMyUnicorn/IService1.cs
+++ PimpMyUnicorn/WCFPimpMyUnicorn/IService1.cs
@@ -11,35 +11,10 @@ namespace WCFPimpMyUnicorn
     [ServiceContract]
     public interface IService1
     {
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
         // TODO: ajoutez vos opérations de service ici
-    }
 
-    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
-    // Vous pouvez ajouter des fichiers XSD au projet. Une fois le projet généré, vous pouvez utiliser directement les types de données qui y sont définis, avec l'espace de noms "WCFPimpMyUnicorn.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+        [OperationContract]
+        bool UpdateDatabase(List<string> queries);
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
